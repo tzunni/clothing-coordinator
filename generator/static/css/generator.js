e6511687b01e95a2c48e3c8d23ed1429e92e9c3b
WonjunUwall <ksm1006ksm7022@gmail.com>
2024-11-15 21:00:50 -0800

    add more stuff in js

--- generator/static/css/generator.js
+++ generator/static/css/generator.js
@@ -1,7 +1,7 @@
 const optionMenu = document.querySelector(".weather-menu"),
-    WeatherSelectBtn = optionMenu.querySelector(".weather-btn"),
-    options = optionMenu.querySelectorAll(".option"),
-    wBtn_text = optionMenu.querySelector(".wBtn-text");
+      WeatherSelectBtn = optionMenu.querySelector(".weather-btn"),
+      options = optionMenu.querySelectorAll(".option"),
+      wBtn_text = optionMenu.querySelector(".wBtn-text");
 
 WeatherSelectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
 
@@ -11,5 +11,23 @@ options.forEach(option =>{
         wBtn_text.innerText = selectedOption;
         
         optionMenu.classList.toggle("active");
+
+    });
+});
+      
+
+const shadeMenu = document.querySelector(".shade-menu"),
+      ShadeSelectionBtn = shadeMenu.querySelector(".shade-btn"),
+      shadeOptions = shadeMenu.querySelectorAll("shade-option"),
+      sBtn_text = shadeMenu.querySelector(".sBtn-text");
+
+ShadeSelectionBtn.addEventListener("click" , () => shadeMenu.classList.toggle("active"));
+
+shadeOptions.forEach(option =>{
+    option.addEventListener("click", () =>{
+        let selectedShadeOption = option.querySelector(".sOption-text").innerText;
+        sBtn_text.innerText = selectedShadeOption;
+
+      SoptonMenu.classList.toggle("active");
     });
 });
