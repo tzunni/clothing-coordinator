abf10bb3fe15cb9c91afde5aee9677ecae95ca9e
Keith Bui <kqbui@csu.fullerton.edu>
2024-11-17 00:11:39 -0800

    Feature Parity between Shade and Weather

--- generator/static/css/generator.js
+++ generator/static/css/generator.js
@@ -5,29 +5,27 @@ const optionMenu = document.querySelector(".weather-menu"),
 
 WeatherSelectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
 
-options.forEach(option =>{
-    option.addEventListener("click", () =>{
+options.forEach(option => {
+    option.addEventListener("click", () => {
         let selectedOption = option.querySelector(".option-text").innerText;
         wBtn_text.innerText = selectedOption;
         
         optionMenu.classList.toggle("active");
-
     });
 });
-      
 
 const shadeMenu = document.querySelector(".shade-menu"),
       ShadeSelectionBtn = shadeMenu.querySelector(".shade-btn"),
-      shadeOptions = shadeMenu.querySelectorAll("shade-option"),
+      shadeOptions = shadeMenu.querySelectorAll(".shade-option"), // Corrected selector
       sBtn_text = shadeMenu.querySelector(".sBtn-text");
 
-ShadeSelectionBtn.addEventListener("click" , () => shadeMenu.classList.toggle("active"));
+ShadeSelectionBtn.addEventListener("click", () => shadeMenu.classList.toggle("active"));
 
-shadeOptions.forEach(option =>{
-    option.addEventListener("click", () =>{
+shadeOptions.forEach(option => {
+    option.addEventListener("click", () => {
         let selectedShadeOption = option.querySelector(".sOption-text").innerText;
         sBtn_text.innerText = selectedShadeOption;
 
-      SoptonMenu.classList.toggle("active");
+        shadeMenu.classList.toggle("active");
     });
 });
